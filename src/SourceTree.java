cf9c604934c634227538d74ced02e14f737a1b86
LimDoKyoung <dokyoung965@naver.com>
2017-01-17 13:19:39 +0900

    int f 정의 및 출력

--- src/SourceTree.java
+++ src/SourceTree.java
@@ -5,12 +5,15 @@ public class SourceTree {
 		int b = 200;
 		int c = 0;
 		int d = 0;
+		int f = 0;
 		c= a+b;
 		d = b-a;
+		f = a*b;
 		
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
 		System.out.println(d);
+		System.out.println(f);
 	}
 }
